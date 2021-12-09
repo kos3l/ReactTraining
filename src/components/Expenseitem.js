823fd32c99facd9ed72ecc26ec655652e2cd3778
kos3l <71730597+kos3l@users.noreply.github.com>
2021-12-09 14:40:30 +0100

    first assignment

--- src/components/Expenseitem.js
+++ src/components/Expenseitem.js
@@ -1,9 +1,13 @@
 import "./Expenseitem.css";
+import ExpenseDate from "./ExpenseDate";
 //all the props are put into one object accessable with a name u want like 'props'
 function ExpenseItem(props) {
+  const month = props.date.toLocaleString("en-Us", { month: "long" });
+  const year = props.date.getFullYear();
+  const day = props.date.toLocaleString("en-Us", { day: "2-digit" });
   return (
     <div className="expense-item">
-      <div>{props.date.toISOString()}</div>
+      <ExpenseDate date={props.date} />
       <div className="expense-item__description">
         <h2>{props.title}</h2>
         <div className="expense-item__price"> $ {props.amount}</div>
