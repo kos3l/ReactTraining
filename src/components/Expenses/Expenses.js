7eb8377e3d8835c8a8cf83be56a3c595f5a4f619
kos3l <71730597+kos3l@users.noreply.github.com>
2022-01-01 20:55:56 +0100

    chapter 5

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,33 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Expenses.css";
 import Card from "../UI/Card";
-import Expenseitem from "./Expenseitem";
+import ExpensesList from "./ExpensesList";
+import ExpensesFilter from "./ExpensesFilter";
+//whenever u use to way binding u create a controlled compontent
+//the compontent is controlled by the parent compontent, the logic resides in ther when listeners are in the controlled one
+//always add key when you map arrays (react cant see a difference between elements in the array and sees that it got longer so it rewrites it which can make bugs, always add a key)
 
 const Expenses = (props) => {
-  return (
-    <Card className="expenses">
-      <Expenseitem
-        title={props.items[0].title}
-        amount={props.items[0].amount}
-        date={props.items[0].date}
-      />
+  const [filteredYear, setFilteredYear] = useState("2020");
 
-      <Expenseitem
-        title={props.items[1].title}
-        amount={props.items[1].amount}
-        date={props.items[1].date}
-      />
-      <Expenseitem
-        title={props.items[2].title}
-        amount={props.items[2].amount}
-        date={props.items[2].date}
-      />
-      <Expenseitem
-        title={props.items[3].title}
-        amount={props.items[3].amount}
-        date={props.items[3].date}
-      />
-    </Card>
+  const savePickedYear = (enteredPickedYear) => {
+    setFilteredYear(enteredPickedYear);
+  };
+
+  const filteredExpenses = props.items.filter((expense) => {
+    return expense.date.getFullYear().toString() === filteredYear;
+  });
+
+  return (
+    <div>
+      <Card className="expenses">
+        <ExpensesFilter selected={filteredYear} onPickedYear={savePickedYear} />
+        <ExpensesList items={filteredExpenses} />
+      </Card>
+    </div>
   );
 };
 
